de748271351279d7a428725e2e72e9c76788d8c9
BricePaquette <brice.paquette@gmail.com>
2022-11-29 14:57:51 -0600

    Converted Index to an MVC design pattern architecture.

---
+++ PersonalSiteV2/PersonalSiteV2.UI.MVC/wwwroot/js/main.js
@@ -0,0 +1,27 @@
+function setColor(color) {
+	let currentColor = document.getElementById('color-changer')
+	//let currentFav = document.getElementById('Favicon')
+	switch (color) {
+		case 'blue':
+			currentColor.setAttribute('href', '/css/colors/blue.css')
+			
+			break;
+		case 'green':
+			currentColor.setAttribute('href', '/css/colors/green.css')
+			
+			break;
+		case 'yellow':
+			currentColor.setAttribute('href', '/css/colors/yellow.css')
+			
+			break;
+		case 'red':
+			currentColor.setAttribute('href', '/css/colors/red.css')
+			
+			break;
+	}
+	localStorage.setItem('jsColor', color)
+	
+}
+if (localStorage.getItem('jsColor') != null) {
+	setColor(localStorage.getItem('jsColor'))
+}
